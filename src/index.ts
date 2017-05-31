f88b08f6722a28bada3f56dcab9ad22889c63727
Carney Claunch III <Twitch3@users.noreply.github.com>
2017-05-31 08:30:09 -0500

    Updated bot access token.
    Updated the DnDBot access token to the proper value (was still using HypeBot's).

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import { TextResponses } from "./bot-modules/text-responses/text-responses";
 import { TextCommands } from "./bot-modules/text-commands/text-commands";
 import { DotaMatches } from "./bot-modules/dota-matches/dota-matches";
 
-const token = "362547656:AAGAl4o2TvPiXEg1HN4XN5C-zf10xdeuRNk";
+const token = "331467600:AAFUKz7Nl8ncw15jtYxWLBzv9CxV65q1Zyk";
 const MyTelegramBot = new TelegramBot(token, { polling: true });
 
 new TextResponses(MyTelegramBot);
