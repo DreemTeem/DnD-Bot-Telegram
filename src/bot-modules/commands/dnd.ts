0a9366982ae45ff84ab4bd116373afb2184958fd
Twitch3 <unconfigured@null.spigotmc.org>
2018-05-16 22:15:03 -0500

    Updates to add Spells command and update NPM packages. - Added new command to DnD command section that searches specifically for   spells. - Added some TODO comments for planned expansions. - Updated some NPM packages.

--- src/bot-modules/commands/dnd.ts
+++ src/bot-modules/commands/dnd.ts
@@ -8,7 +8,12 @@ export class DnDCommands {
   constructor(botReference: TelegramBot) {
     this.DnDBot = botReference;
     this.setDnDCommand();
+    this.setSpellCommand();
   }
+
+  // TODO: Add integration for the following sites:
+  // http://dnd.arkalseif.info/lookup/search.php - General DnD stuff (better than current SRD)
+  // http://monsterfinder.dndrunde.de/ - Monster finder
 
   private setDnDCommand(): void {
     this.DnDBot.onText(/^\/dnd/i, (msg: any, match: any): void => {
@@ -25,6 +30,21 @@ export class DnDCommands {
     });
   }
 
+  private setSpellCommand(): void {
+    this.DnDBot.onText(/^\/spells/i, (msg: any, match: any): void => {
+      const commandArray: string[] = msg.text.split(" ");
+      if (commandArray.length === 1) {
+        this.getGenericResponse(msg.chat.id);
+      } else {
+        switch (commandArray[1]) {
+          default:
+            this.getSpellSearch(msg.chat.id, commandArray.splice(1, commandArray.length).join(" "));
+            break;
+        }
+      }
+    });
+  }
+
   private getGenericSearch(messageId: number, searchQuery: string): void {
     const uriRoot: string = "http://www.dandwiki.com";
     const uriSearch: string = encodeURIComponent(searchQuery);
@@ -33,8 +53,33 @@ export class DnDCommands {
         const $: any = this.cheerio.load(response.body);
         const mainResults: any = $(".mw-search-results").eq(0).find("li a");
         let message: string = "<b>Basic DnD Search Results</b>\n==============================\n";
+        // TODO: Add cases to provide better message when no results match searchQuery.
         mainResults.each((index: number, element: any) => {
           message += "  <a href=\"" + uriRoot + element.attribs.href + "\">" + element.attribs.title + "</a>\n";
+        });
+        this.DnDBot.sendMessage(messageId, message, { parse_mode: "HTML", disable_web_page_preview: true });
+      } else if (error) {
+        this.DnDBot.sendMessage(messageId, "Sorry, no results were returned.", { parse_mode: "HTML", disable_web_page_preview: true });
+      } else {
+        this.DnDBot.sendMessage(messageId, "Sorry, an unknown error has occured.", { parse_mode: "HTML", disable_web_page_preview: true });
+      }
+    });
+  }
+
+  private getSpellSearch(messageId: number, searchQuery: string): void {
+    const uriRoot: string = "https://thebombzen.com/";
+    const uriSearch: string = encodeURIComponent(searchQuery);
+    this.request.get(uriRoot + "grimoire/", (error: any, response: any, body: any) => {
+      if (response) {
+        const $: any = this.cheerio.load(response.body);
+        const mainResults: any = $("ul.post-list.jetsContent li a");
+        let message: string = "<b>Basic DnD Search Results</b>\n==============================\n";
+        // TODO: Add cases to provide better message when no results match searchQuery.
+        mainResults.each((index: number, element: any) => {
+          const spellName: string = $(element).text();
+          if (spellName.toLowerCase().includes(searchQuery.toLowerCase())) {
+            message += "  <a href=\"" + uriRoot + element.attribs.href + "\">" + spellName + "</a>\n";
+          }
         });
         this.DnDBot.sendMessage(messageId, message, { parse_mode: "HTML", disable_web_page_preview: true });
       } else if (error) {
