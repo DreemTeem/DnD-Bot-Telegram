af03aaafeff92f0350c9f295b068b4792e774bb6
BuildTools <unconfigured@null.spigotmc.org>
2019-11-10 16:29:35 -0600

    Added Yeet script to respond to the growing of the king

--- src/bot-modules/yeet/yeet.ts
+++ src/bot-modules/yeet/yeet.ts
@@ -10,7 +10,6 @@ export class YeetResponses {
 
   private setBasicYeetResponses(): void {
     this.GBot.onText(/yee+t/i, (msg: any, match: any): void => {
-      console.log(msg);
       this.GBot.sendMessage(msg.chat.id, 'The Yeet King Grows...');
     });
   }
