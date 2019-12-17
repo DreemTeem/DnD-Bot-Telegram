d16208869a957d12395b075459a74de551b4373e
BuildTools <unconfigured@null.spigotmc.org>
2019-12-17 17:52:19 -0600

    Added flip command and images for coin

--- src/bot-modules/commands/roll.ts
+++ src/bot-modules/commands/roll.ts
@@ -1,26 +1,35 @@
 const Bot = require('node-telegram-bot-api');
 
 export class RollCommands {
-  private HBot;
+  private GBot;
 
   constructor(botReference) {
-    this.HBot = botReference;
+    this.GBot = botReference;
     this.setRollCommand();
   }
 
   private setRollCommand(): void {
-    this.HBot.onText(/^\/roll/i, (msg: any, match: any): void => {
+    this.GBot.onText(/^\/roll/i, (msg: any, match: any): void => {
 
       const commandArray: string[] = msg.text.split(" ");
       const verbose: boolean = msg.text.indexOf(" -v") > 0;
       const diceMatch: string[] = msg.text.match(/([1-9][0-9]*)d([1-9][0-9]*)/);
 
       if (!diceMatch) {
-        this.HBot.sendMessage(msg.chat.id, this.getRollMessage(6, 1, verbose));
+        this.GBot.sendMessage(msg.chat.id, this.getRollMessage(6, 1, verbose));
       } else {
         const numberOfDice: number = parseInt(diceMatch[1], 10);
         const diceSize: number = parseInt(diceMatch[2], 10);
-        this.HBot.sendMessage(msg.chat.id, this.getRollMessage(diceSize, numberOfDice, verbose));
+        this.GBot.sendMessage(msg.chat.id, this.getRollMessage(diceSize, numberOfDice, verbose));
+      }
+    });
+
+    this.GBot.onText(/^\/flip/i, (msg: any, match: any): void => {
+      const result = Math.floor(Math.random() * 2) + 1;
+      if (result === 1) {
+        this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/heads.png', {caption: 'Heads'});
+      } else {
+        this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/tails.png', {caption: 'Tails'});
       }
     });
   }
