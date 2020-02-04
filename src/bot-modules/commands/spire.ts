056feeecb4df5d7839b05d5350da67652c033da5
BuildTools <unconfigured@null.spigotmc.org>
2020-02-04 14:32:13 -0600

    Debug message added to see why deployed bot isn't working

--- src/bot-modules/commands/spire.ts
+++ src/bot-modules/commands/spire.ts
@@ -28,29 +28,23 @@ export class SpireCommands {
   }
 
   private setSpireCommand(): void {
-    console.log('\n\nSETTING SPIRE\n\n');
     this.GBot.onText(/^\/spire/i, (msg: any, match: any): void => {
-      console.log('\n\nSPIRE COMMAND THROWN\n\n')
       const result = Math.floor(Math.random() * 4);
       this.GBot.sendMessage(msg.chat.id, "RESULT " + result);
-      // switch (result) {
-      //   case 0:
-      //     this.GBot.sendMessage(msg.chat.id, "RESULT " + result);
-      //     // this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/ironclad.jpg', { caption: SpireCommands.characters.ironclad.intro });
-      //     break;
-      //   case 1:
-      //     this.GBot.sendMessage(msg.chat.id, "RESULT " + result);
-      //     // this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/silent.jpg', { caption: SpireCommands.characters.silent.intro });
-      //     break;
-      //   case 2:
-      //     this.GBot.sendMessage(msg.chat.id, "RESULT " + result);
-      //     // this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/defect.jpg', { caption: SpireCommands.characters.defect.intro });
-      //     break;
-      //   case 3:
-      //     this.GBot.sendMessage(msg.chat.id, "RESULT " + result);
-      //     // this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/watcher.jpg', { caption: SpireCommands.characters.watcher.intro });
-      //     break;
-      // }
+      switch (result) {
+        case 0:
+          this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/ironclad.jpg', { caption: SpireCommands.characters.ironclad.intro });
+          break;
+        case 1:
+          this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/silent.jpg', { caption: SpireCommands.characters.silent.intro });
+          break;
+        case 2:
+          this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/defect.jpg', { caption: SpireCommands.characters.defect.intro });
+          break;
+        case 3:
+          this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/watcher.jpg', { caption: SpireCommands.characters.watcher.intro });
+          break;
+      }
     });
   }
 }
