0c529fb5754027fb146951b8ab897924e6d1c2a7
BuildTools <unconfigured@null.spigotmc.org>
2020-08-05 17:38:11 -0500

    Updated Spire Command - Changed how the challenge flags are used (Now you only need one of them to enable challenges. Using -gc instead of -c will simply enable the generic challenges). - Added sneckoChallanges to allow separate logic to make sure you don't get both challenges (which contradict each other). - Added uniqueChallenges array to characters to allow similar logic to sneckoChallenges for character specific challenges that can't be triggered at the same time. - Added more challenges to both characters and generic sections.

--- src/bot-modules/commands/spire.ts
+++ src/bot-modules/commands/spire.ts
@@ -8,45 +8,95 @@ export class SpireCommands {
       intro: "IT'S TIME FOR THE YEET KING HIMSELF. \n LET'S GET REDY TO FLEX OUR WAY TO FREEDOM. \n I R O N C L A D",
       imageName: "ironclad.jpg",
       challenges: [
-        "GRIT YOUR TOUGH - IT'S TIME TO TAKE ALL EXHAUST CARDS",
-        "MUSCLE MILK \"GET SWOLE\" - YOU NOW HAVE TO TAKE ALL FLEX CARDS"
-      ]
+        "GRIT YOUR TEETH - IT'S TIME TO TAKE ALL EXHAUST CARDS.",
+        "IS THAT HELL YOU SMELL? - YOU HAVE TO TAKE BRIMSTONE! SAY HI TO THE BIRDS FOR ME.",
+        "MUSCLE MILK \"GET SWOLE\" - YOU NOW HAVE TO TAKE ALL FLEX CARDS.",
+        "THROW YOUR WEIGHT AROUND - YOU MUST BUILD A BODY SLAM DECK.",
+        "WHILE YOU WERE PLAYING OTHER CHARACTERS I WAS STUDYING THE BLADE - TIME FOR A PERFECTED STRIKE RUN.",
+        "I'M MORE INTO CARDIO - YOU ARE NOT ALLOWED TO TAKE ANY CARDS OR POTIONS THAT INCREASE STRENGTH."
+      ],
+      uniqueChallenges: []
     },
     silent: {
       intro: "HUSH YOUR MOUTH IT'S TIME TO POISON SOME FOOLS. \n LEAVE THE SHIVS BEHIND AND BECOME A SPOOKY GHOST. \n S I L E N T",
       imageName: "silent.jpg",
       challenges: [
         "NOTHING PERSONAL, KID - IT'S TIME FOR A SHIV DECK. TAKE ALL SHIVS AND SHIV POWERS.",
-        "YOU ARE A SPOOKY GHOST - YOU MUST TAKE ALL AVAILABLE APPARITION"
-      ]
+        "YOU ARE A SPOOKY GHOST - YOU MUST TAKE ALL AVAILABLE APPARITION.",
+        "LET THEM COME TO YOU - CALTROPS MUST NOW BE TAKEN.",
+        "REMOVING TOXICITY FROM MY LIFE - YOU AREN'T ALLOWED TO PICK UP CARDS OR POTIONS THAT CAUSE POISON.",
+        "EXTREMELY SHOWY - IF GRAND FINALE IS AVAILABLE, YOU MUST TAKE IT."
+      ],
+      uniqueChallenges: []
     },
     defect: {
       intro: "WE'RE GONNA CLAW OUR WAY TO VICTORY WITH THIS ONE, BOYS. \n DEFRAG YOUR BRAINS AND WISH UPON A STAR FOR CALIPERS. \n D E F E C T",
       imageName: "defect.jpg",
       challenges: [
-        "ZIP ZAP MOTHERFUCKER - IT'S TIME TO \"FOCUS\" ON LIGHTNING ORBS",
-        "BABY IT'S COLD INSIDE MY HEART - IT'S TIME TO \"FOCUS\" ON FROST ORBS",
-        "DARKNESS CONSUMES ME - IT'S TIME TO \"FOCUS\" ON DARK ORBS",
-        "EVER SEEN \"TOY STORY\"? - YOU MUST TAKE ALL CLAWS"
+        "EVER SEEN \"TOY STORY\"? - YOU MUST TAKE ALL CLAWS.",
+        "STACK OVERFLOW - IF RECURSION IS AVAILABLE YOU MUST TAKE IT.",
+        "ONE SHOT ONE KILL - YOU MUST TAKE ALL BULLSEYES.",
+        "ONE FOR ALL! - YOUR DECK MUST BE THEMED AROUND 0-COST CARDS.",
+        "I'MA FIREN MAH LAZAR - IF YOU SEE A HYPERBEAM, YOU HAVE TO TAKE IT.",
+        "WHO LET SEPHEROTH PLAY? - TIME TO TAKE ALL METEOR STRIKES."
+      ],
+      uniqueChallenges: [
+        {
+          challengeName: "ORBS",
+          challenges: [
+            "ZIP ZAP MOTHERFUCKER - IT'S TIME TO \"FOCUS\" ON LIGHTNING ORBS.",
+            "BABY IT'S COLD INSIDE MY HEART - IT'S TIME TO \"FOCUS\" ON FROST ORBS.",
+            "DARKNESS CONSUMES ME - IT'S TIME TO \"FOCUS\" ON DARK ORBS."
+          ]
+        },
+        {
+          challengeName: "ORB_GEN",
+          challenges: [
+            "PASSIVE AGRESSIVE - YOU CAN'T GENERATE ORBS FROM ATTACK CARDS.",
+            "AGRESSIVE MODE - YOU CAN'T GENERATE ORBS FROM SKILL CARDS (NOT COUNTING YOUR FIRST ZAP)."
+          ]
+        }
       ]
     },
     watcher: {
       intro: "HOW THE FUCK DOES THIS CHARACTER WORK AGAIN? \n WHO CARES, JUST PROSTRATE YOUR WAY TO VICTORY. \n W A T C H E R",
       imageName: "watcher.jpg",
       challenges: [
-        "BOW DOWN - YOU HAVE TO TAKE ALL PROSTRATES",
-        "THE END IS NIGH - IF BLASPHEMOUS POPS UP, YOU GOTTA TAKE IT",
-        "IS THE PRESSURE TOO MUCH FOR YOU TO HANDLE? - TIME TO BUILD A PRESSURE POINTS DECK"
-      ]
+        "BOW DOWN - YOU HAVE TO TAKE ALL PROSTRATES.",
+        "THE END IS NIGH - IF BLASPHEMOUS POPS UP, YOU GOTTA TAKE IT.",
+        "IS THE PRESSURE TOO MUCH FOR YOU TO HANDLE? - TIME TO BUILD A PRESSURE POINTS DECK.",
+        "ALPHA FEMALE - TAKE ALL OFFERED ALPHAS.",
+        "PALADIN MODE - TAKE ANY CARD THAT ALLOWS YOU TO GENERATE SMITE CARDS.",
+        "MARK IT ZERO - YOU ARE NOW REQUIRED TO TAKE ALL BOWLING BASHES.",
+        "I CAN SEE THE FUTURE - TAKE ALL CARDS THAT GRANT YOU 'SCRY'.",
+        "EMPTY YOURSELF - TAKE ALL EMPTY BODY, EMPTY FIST, AND EMPTY MIND CARDS.",
+        "HUNGRY? - YOU MUST TAKE AT LEAST 2 FASTING CARDS IF THEY SHOW UP.",
+        "THEY TOOK URRR JERBS - TAKE ALL FOREIGN INFLUENCE CARDS.",
+        "BIGGER HAND IS BETTERER - IF THE CARD HAS 'RETAIN' ... TAKE IT.",
+        "I AM... THE LAW - YOU MUST TAKE JUDGEMENT CARDS."
+      ],
+      uniqueChallenges: []
     }
   }
 
   public static genericChallenges = [
-    "PRAISE SNECKO - YOU MUST TAKE SNECKO EYE NO MATTER WHAT IF IT SHOWS",
-    "HERECY - YOU ARE FORBIDDEN FROM TAKING SNECKO EYE OR USING SNECKO OIL",
     "SO ... COLORFUL - YOU MUST TAKE PRISMATIC SHARD IF IT APPEARS",
-    "FOLLOW DEREK'S ADVICE - IF THERE IS A CHANCE OR OPPORTUNITY FOR A CURSE, YOU MUST TAKE IT",
-    "DIG! - IF YOU SEE THE SHOVEL, YOU MUST TAKE IT. IF YOU HAVE THE SHOVEL, YOU MUST DIG."
+    "FOLLOW DEREK'S ADVICE - IF THERE IS A CHANCE OR OPPORTUNITY FOR A CURSE, YOU MUST TAKE IT.",
+    "DIG! - IF YOU SEE THE SHOVEL, YOU MUST TAKE IT. IF YOU HAVE THE SHOVEL, YOU MUST DIG.",
+    "YOU ARE THE BOMB! - NOT REALLY, BUT IF YOU SEE THE BOMB YOU MUST TAKE IT. MAKE MR TORGUE PROUD!",
+    "DRINK UP! - TAKE ALL POTION RELATED RELICS AND CARDS.",
+    "HEALTHY BOI! - TAKE ALL CHANCES TO INCREASE YOUR MAX HP (AS LONG AS IT DOESN'T KILL YOU).",
+    "YOU GOT SOME BLOAT THERE - YOU CAN'T SKIP CARDS AT THE ENDS OF FIGHTS ANYMORE.",
+    "MR POPULAR - YOU CAN NO LONGER JUST LEAVE AN EVENT WITHOUT DOING SOMETHING.",
+    "THE MOST RIGHT WAY - YOU CAN'T SCROLL UP ON THE MAP. PRAY FOR WING BOOTS.",
+    "MAXIMUM GREED - ALL CARDS AND RELICS THAT INCREASE YOUR GOLD MUST BE TAKEN.",
+    "EMBRACE THE INSANITY - YOU MUST TAKE ALL MADNESS CARDS.",
+    "CONTROLLED CHAOS - YOU MUST PICK UP AT LEAST ONE MAYHEM CARD IF AVAILABLE."
+  ];
+
+  public static sneckoChallenges = [
+    "PRAISE SNECKO - YOU MUST TAKE SNECKO EYE NO MATTER WHAT IF IT SHOWS.",
+    "HERECY - YOU ARE FORBIDDEN FROM TAKING SNECKO EYE OR USING SNECKO OIL."
   ];
 
   constructor(botReference) {
@@ -77,7 +127,7 @@ export class SpireCommands {
       const promise = this.GBot.sendPhoto(msg.chat.id, __dirname + '/../../../assets/spire/' + character.imageName, { caption: character.intro });
 
       promise.then(() => {
-        if (challengeIndex > 0) {
+        if (challengeIndex > 0 || genericChallengeIndex > 0) {
           this.GBot.sendMessage(msg.chat.id, 'Time for a\nCHALLENGE...');
           let numChallenges = 1;
           if (challengeIndex < commandArray.length - 1) {
@@ -86,12 +136,32 @@ export class SpireCommands {
               numChallenges = nextIndexNumber;
             }
           }
+
+          if (numChallenges === 1 && genericChallengeIndex < commandArray.length - 1) {
+            if (genericChallengeIndex < commandArray.length - 1) {
+              const nextIndexNumber = parseInt(commandArray[genericChallengeIndex + 1], 10);
+              if (!isNaN(nextIndexNumber)) {
+                numChallenges = nextIndexNumber;
+              }
+            }
+          }
           const addedChallengeIndexes = [];
           const addedChallenges = [];
           let availableChallenges = character.challenges;
 
+          if (character.uniqueChallenges && character.uniqueChallenges.length) {
+            character.uniqueChallenges.forEach((challengeObj) => {
+              if (challengeObj.challenges) {
+                const uniqueChallengeIndex = Math.floor(Math.random() * challengeObj.challenges.length);
+                availableChallenges.push(challengeObj.challenges[uniqueChallengeIndex]);
+              }
+            });
+          }
+
           if (genericChallengeIndex > 0) {
             availableChallenges = availableChallenges.concat(SpireCommands.genericChallenges);
+            const sneckoIndex = Math.floor(Math.random() * 2);
+            availableChallenges.push(SpireCommands.sneckoChallenges[sneckoIndex]);
           }
 
           if (numChallenges > availableChallenges.length) {
